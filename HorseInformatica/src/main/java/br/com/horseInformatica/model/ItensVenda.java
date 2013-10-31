ef885a755842ef7ae2cde72cc94b11b747f0a649
Jociel <jociel_7@hotmail.com>
2013-10-31 18:16:05 -0300

    Commit de rotina

--- HorseInformatica/src/main/java/br/com/horseInformatica/model/ItensVenda.java
+++ HorseInformatica/src/main/java/br/com/horseInformatica/model/ItensVenda.java
@@ -3,6 +3,8 @@ package br.com.horseInformatica.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -11,9 +13,9 @@ public class ItensVenda implements Serializable{
 
 	private static final long serialVersionUID = 4118158237574641361L;
 	
-	
+	@ManyToMany
+	@JoinColumn(name = "produto")
 	private Produto produto;
-	
 	private Venda venda;
 
 }
