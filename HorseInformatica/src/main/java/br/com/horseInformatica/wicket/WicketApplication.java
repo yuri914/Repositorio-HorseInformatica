48c1cd28f5cd144af2674828270aed189292767d
Jociel <jociel_7@hotmail.com>
2013-10-31 00:23:09 -0300

    Criação do pacote wicket

--- HorseInformatica/src/main/java/br/com/horseInformatica/wicket/WicketApplication.java
+++ HorseInformatica/src/main/java/br/com/horseInformatica/wicket/WicketApplication.java
@@ -1,7 +1,7 @@
-package br.com.horseInformatica;
-
+package br.com.horseInformatica.wicket;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import br.com.horseInformatica.HomePage;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
