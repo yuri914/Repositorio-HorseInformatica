5e3d31eee863273824f208ecca0132a1f05f9e2c
jose.almeida <jose.almeida@FORDSKDOPGPS021.castmeta.local>
2013-11-25 13:44:14 -0300

    Atualização de clientes passa a ser na base page com a criação do botão de ações que possui Configurações da conta e Sair

--- HorseInformatica/src/main/java/br/com/horseInformatica/util/relatorios/RelatorioFactory.java
+++ HorseInformatica/src/main/java/br/com/horseInformatica/util/relatorios/RelatorioFactory.java
@@ -1,62 +1,31 @@
 package br.com.horseInformatica.util.relatorios;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.jasperreports.engine.JRExporter;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.engine.export.JRPdfExporterParameter;
-import net.sf.jasperreports.engine.util.JRLoader;
+public class RelatorioFactory
+{
 
-public class RelatorioFactory {
-	
-	public static void gerarRelatorio (HttpServletResponse httpResponse, 
-	List<?> lista, String reportURL, Map<String, Object> parametros, String PdfFileName) {
-		
-		HttpServletResponse response =  httpResponse;
-		OutputStream os = null;
-		
-		try{
-			os = response.getOutputStream();
-			response.setHeader("Content-disposition", "attachment; filename="+ PdfFileName);
-			
-			//Carrega o relatório localizado no caminho especificado.
-			JasperReport jr = (JasperReport) JRLoader.loadObject(reportURL);
-			
-			//Popula o Bean com a lista informada pro parametro.
-			JRBeanCollectionDataSource jrBean = new JRBeanCollectionDataSource(lista);
-			
-			//Preenche o relatório com os parametros e o Bean.
-			JasperPrint jsPrint = JasperFillManager.fillReport(jr, parametros, jrBean);
-			
-			JRExporter exporter = new JRPdfExporter();
-			exporter.setParameter(JRPdfExporterParameter.JASPER_PRINT, jsPrint);
-			exporter.setParameter(JRPdfExporterParameter.OUTPUT_STREAM, os);
-			exporter.setParameter(JRPdfExporterParameter.CHARACTER_ENCODING, "UTF-8");
-			exporter.exportReport();
-			
-			System.out.println("Relatório gerado com sucesso.");
-		} catch(Exception e){
-			e.printStackTrace();
-			System.out.println("Ocorreu um erro ao gerar o relatório.");
-		} finally{
-			try {
-				if (os != null){
-					os.flush();
-					os.close();
-				}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-	}	
+   public static void gerarRelatorio(HttpServletResponse httpResponse, List<?> lista,
+      String reportURL, Map<String, Object> parametros, String PdfFileName)
+   {
+      /*
+       * HttpServletResponse response = httpResponse; OutputStream os = null; try{ os =
+       * response.getOutputStream(); response.setHeader("Content-disposition",
+       * "attachment; filename="+ PdfFileName); //Carrega o relatório localizado no caminho
+       * especificado. JasperReport jr = (JasperReport) JRLoader.loadObject(reportURL); //Popula o
+       * Bean com a lista informada pro parametro. JRBeanCollectionDataSource jrBean = new
+       * JRBeanCollectionDataSource(lista); //Preenche o relatório com os parametros e o Bean.
+       * JasperPrint jsPrint = JasperFillManager.fillReport(jr, parametros, jrBean); JRExporter
+       * exporter = new JRPdfExporter(); exporter.setParameter(JRPdfExporterParameter.JASPER_PRINT,
+       * jsPrint); exporter.setParameter(JRPdfExporterParameter.OUTPUT_STREAM, os);
+       * exporter.setParameter(JRPdfExporterParameter.CHARACTER_ENCODING, "UTF-8");
+       * exporter.exportReport(); System.out.println("Relatório gerado com sucesso."); }
+       * catch(Exception e){ e.printStackTrace();
+       * System.out.println("Ocorreu um erro ao gerar o relatório."); } finally{ try { if (os !=
+       * null){ os.flush(); os.close(); } } catch (IOException e) { e.printStackTrace(); } }
+       */
+   }
+
+}
--- HorseInformatica/src/main/java/br/com/horseInformatica/view/administrador/AdmistradorAtualizaClienteForm.java
+++ HorseInformatica/src/main/java/br/com/horseInformatica/view/administrador/AdmistradorAtualizaClienteForm.java
@@ -2,7 +2,6 @@ package br.com.horseInformatica.view.administrador;
 
 import java.util.Arrays;
 import java.util.Date;
-
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
@@ -16,150 +15,191 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-
 import br.com.horseInformatica.model.Cliente;
 import br.com.horseInformatica.service.AuxiliarService;
 import br.com.horseInformatica.to.EnderecoTO;
 import br.com.horseInformatica.util.enumerations.EnumSexo;
+import br.com.horseInformatica.view.base.BasePage;
 
-public abstract class AdmistradorAtualizaClienteForm extends Form<Cliente> {
+public abstract class AdmistradorAtualizaClienteForm extends Form<Cliente>
+{
 
-	private static final long serialVersionUID = 2535896341184251584L;
+   private static final long serialVersionUID = 2535896341184251584L;
 
-	
-	
-	private FeedbackPanel feedback;
-	
-	//Dados Pessoais
-	private TextField<String> nome;
-	private RadioChoice<EnumSexo> genero;
-	private DateTextField nascimento;
-	private TextField<String> cpf;
-	private TextField<String> rg;
+   private final FeedbackPanel feedback;
 
-	//Contato
-	private TextField<String> email;
-	private TextField<String> telefone;
-	private TextField<Integer> ddd;
-	private TextField<Integer> cep;
-	private TextField<String> logradouro;
-	private TextField<String> bairro;
-	private TextField<String> cidade;
-	private TextField<String> estado;
+   // Dados Pessoais
+   private final TextField<String> nome;
+   private final RadioChoice<EnumSexo> genero;
+   private final DateTextField nascimento;
+   private final TextField<String> cpf;
+   private final TextField<String> rg;
 
+   // Contato
+   private final TextField<String> email;
+   private final TextField<String> telefone;
+   private final TextField<Integer> ddd;
+   private final TextField<Integer> cep;
+   private final TextField<String> logradouro;
+   private final TextField<String> bairro;
+   private final TextField<String> cidade;
+   private final TextField<String> estado;
+   private final TextField<String> login;
+   private final TextField<String> senhaAtual;
+   private final TextField<String> senha;
+   private final AjaxButton btConfirmar;
+   private Cliente clienteAtualizar;
 
+   public AdmistradorAtualizaClienteForm(String id, final Cliente cliente)
+   {
+      super(id);
+      // final Cliente clienteSessao = (Cliente) getSession().getAttribute("clienteSessao");
 
-	private AjaxButton btConfirmar;
-	
-	public AdmistradorAtualizaClienteForm(String id, final Cliente cliente) {
-		super(id);
-		
-	
-		feedback = new FeedbackPanel("mensagem");
-		feedback.setOutputMarkupPlaceholderTag(true);
-		add(feedback);
+      setClienteAtualizar(cliente);
+      feedback = new FeedbackPanel("mensagem");
+      feedback.setOutputMarkupPlaceholderTag(true);
+      add(feedback);
 
-		
-		nome = new TextField<String>("nome");
-		nome.setModel(new PropertyModel<String>(cliente, "nome"));
-		add(nome);
+      nome = new TextField<String>("nome");
+      nome.setModel(new PropertyModel<String>(cliente, "nome"));
+      add(nome);
 
-		cpf = new TextField<String>("cpf");
-		cpf.setModel(new PropertyModel<String>(cliente, "cpf"));
-		add(cpf);
+      cpf = new TextField<String>("cpf");
+      cpf.setModel(new PropertyModel<String>(cliente, "cpf"));
+      add(cpf);
 
-		rg = new TextField<String>("rg");
-		rg.setModel(new PropertyModel<String>(cliente, "rg"));
-		add(rg);
+      rg = new TextField<String>("rg");
+      rg.setModel(new PropertyModel<String>(cliente, "rg"));
+      add(rg);
 
-		nascimento = new DateTextField("dataNascimento", new PropertyModel<Date>(cliente, "dataNascimento"), new StyleDateConverter("S-", true));
+      nascimento =
+         new DateTextField("dataNascimento", new PropertyModel<Date>(cliente, "dataNascimento"),
+            new StyleDateConverter("S-", true));
 
-		DatePicker datePicker = new DatePicker();
-		datePicker.setShowOnFieldClick(true);
-		datePicker.setAutoHide(true);
-		nascimento.add(datePicker);
-		add(nascimento);
+      DatePicker datePicker = new DatePicker();
+      datePicker.setShowOnFieldClick(true);
+      datePicker.setAutoHide(true);
+      nascimento.add(datePicker);
+      add(nascimento);
 
-		genero = new RadioChoice<EnumSexo>("genero");
-		genero.setChoices(Arrays.asList(EnumSexo.values()));
-		genero.setChoiceRenderer(new ChoiceRenderer<EnumSexo>("descricao"));
-		genero.setModel(new PropertyModel<EnumSexo>(cliente, "sexo"));
-		genero.setSuffix("  ");
-		add(genero);
+      genero = new RadioChoice<EnumSexo>("genero");
+      genero.setChoices(Arrays.asList(EnumSexo.values()));
+      genero.setChoiceRenderer(new ChoiceRenderer<EnumSexo>("descricao"));
+      genero.setModel(new PropertyModel<EnumSexo>(cliente, "sexo"));
+      genero.setSuffix("  ");
+      add(genero);
 
-		email = new TextField<String>("email");
-		email.setModel(new PropertyModel<String>(cliente.getContato(), "email"));
-		add(email);
+      login = new TextField<String>("login");
+      login.setModel(new PropertyModel<String>(cliente, "login"));
+      add(login);
 
-		telefone = new TextField<String>("telefone");
-		telefone.setModel(new PropertyModel<String>(cliente.getContato(), "telefone"));
-		add(telefone);
+      senhaAtual = new TextField<String>("senhaAtual");
+      senhaAtual.setModel(new Model(""));
+      add(senhaAtual);
 
-		/*ddd = new DropDownChoice<Integer>("ddd");
-		List<Integer> listaDdd = new ArrayList<Integer>(Arrays.asList(21, 11, 71, 85, 51, 31));
-		ddd.setChoices(listaDdd);
-		ddd.setModel(new PropertyModel<Integer>(cliente.getContato(), "ddd"));*/
-		
-		ddd = new TextField<Integer>("ddd");
-		ddd.setModel(new PropertyModel<Integer>(cliente.getContato(), "ddd"));
-		add(ddd);
+      senha = new TextField<String>("senha");
+      senha.setModel(new Model(""));
+      add(senha);
 
-		cep = new TextField<Integer>("cep");
-		cep.setModel(new PropertyModel<Integer>(cliente.getContato().getEndereco(), "cep"));
-		cep.add(new AjaxFormComponentUpdatingBehavior("onblur") {
+      email = new TextField<String>("email");
+      email.setModel(new PropertyModel<String>(cliente.getContato(), "email"));
+      add(email);
 
-			private static final long serialVersionUID = -1358699233483750529L;
+      telefone = new TextField<String>("telefone");
+      telefone.setModel(new PropertyModel<String>(cliente.getContato(), "telefone"));
+      add(telefone);
 
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				EnderecoTO enderecoCep = AuxiliarService.getEnderecoWebService(cep.getModelObject());
-				if (enderecoCep != null) {
-					logradouro.setModel(Model.of(enderecoCep.getLogradouro()));
-					bairro.setModel(Model.of(enderecoCep.getBairro()));
-					estado.setModel(Model.of(enderecoCep.getEstado()));
-					cidade.setModel(Model.of(enderecoCep.getCidade()));
-				} else {
-					error("Cep não encontrado.");
-				}
-				target.add(logradouro, bairro, estado, cidade, feedback);
-			}
-		});
-		add(cep);
+      ddd = new TextField<Integer>("ddd");
+      ddd.setModel(new PropertyModel<Integer>(cliente.getContato(), "ddd"));
+      add(ddd);
 
-		logradouro = new TextField<String>("logradouro");
-		logradouro.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "logradouro"));
-		add(logradouro);
+      cep = new TextField<Integer>("cep");
+      cep.setModel(new PropertyModel<Integer>(cliente.getContato().getEndereco(), "cep"));
+      cep.add(new AjaxFormComponentUpdatingBehavior("onblur")
+      {
 
-		bairro = new TextField<String>("bairro");
-		bairro.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "bairro"));
-		add(bairro);
+         private static final long serialVersionUID = -1358699233483750529L;
 
-		cidade = new TextField<String>("cidade");
-		cidade.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "cidade"));
-		add(cidade);
+         @Override
+         protected void onUpdate(AjaxRequestTarget target)
+         {
+            EnderecoTO enderecoCep = AuxiliarService.getEnderecoWebService(cep.getModelObject());
+            if (enderecoCep != null)
+            {
+               logradouro.setModel(Model.of(enderecoCep.getLogradouro()));
+               bairro.setModel(Model.of(enderecoCep.getBairro()));
+               estado.setModel(Model.of(enderecoCep.getEstado()));
+               cidade.setModel(Model.of(enderecoCep.getCidade()));
+            }
+            else
+            {
+               error("Cep não encontrado.");
+            }
+            target.add(logradouro, bairro, estado, cidade, feedback);
+         }
+      });
+      add(cep);
 
-		estado = new TextField<String>("estado");
-		estado.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "estado"));
-		add(estado);
-		
-		
-		btConfirmar = new AjaxButton("confirmar") {
+      logradouro = new TextField<String>("logradouro");
+      logradouro.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(),
+         "logradouro"));
+      add(logradouro);
 
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				
-				 atualizarCliente(cliente);
-				 String menssagem = "Cliente atualizado com sucesso !";
-				 setResponsePage(new AdministradorClientePage());
-			}
+      bairro = new TextField<String>("bairro");
+      bairro.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "bairro"));
+      add(bairro);
 
-		
-		};
-		add(btConfirmar);
+      cidade = new TextField<String>("cidade");
+      cidade.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "cidade"));
+      add(cidade);
 
-	}
+      estado = new TextField<String>("estado");
+      estado.setModel(new PropertyModel<String>(cliente.getContato().getEndereco(), "estado"));
+      add(estado);
 
-	protected abstract void atualizarCliente(Cliente cliente);
+      btConfirmar = new AjaxButton("confirmar")
+      {
+
+         private static final long serialVersionUID = 1L;
+
+         @Override
+         protected void onSubmit(AjaxRequestTarget target, Form<?> form)
+         {
+            if (senhaAtual.getModelObject().equals(getClienteAtualizar().getSenha()))
+            {
+               cliente.setSenha(senha.getModelObject());
+               atualizarCliente(cliente);
+               target.appendJavaScript("alert('Cliente atualizado com sucesso!')");
+
+               setResponsePage(new BasePage());
+            }
+            else
+            {
+               target.appendJavaScript("alert('Senha atual informada inválida!')");
+            }
+            target.add(feedback);
+         }
+
+      };
+      add(btConfirmar);
+
+   }
+
+   public Cliente getClienteAtualizar()
+   {
+      if (clienteAtualizar == null)
+      {
+         clienteAtualizar = new Cliente();
+
+      }
+
+      return clienteAtualizar;
+   }
+
+   public void setClienteAtualizar(Cliente clienteAtualizar)
+   {
+      this.clienteAtualizar = clienteAtualizar;
+   }
+
+   protected abstract void atualizarCliente(Cliente cliente);
 }
