6dea882044b3cd9579c1ff47e6748d2f42e5a625
jose.almeida <jose.almeida@FORDSKDOPGPS021.castmeta.local>
2013-11-26 13:00:24 -0300

    Commit diario

--- HorseInformatica/src/main/java/br/com/horseInformatica/view/administrador/AdministradorAtualizaClientePage.java
+++ HorseInformatica/src/main/java/br/com/horseInformatica/view/administrador/AdministradorAtualizaClientePage.java
@@ -2,32 +2,37 @@ package br.com.horseInformatica.view.administrador;
 
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-
 import br.com.horseInformatica.model.Cliente;
 import br.com.horseInformatica.service.ServiceCliente;
+import br.com.horseInformatica.view.base.BasePage;
 
-public class AdministradorAtualizaClientePage extends WebPage {
+public class AdministradorAtualizaClientePage extends BasePage
+{
 
-	private static final long serialVersionUID = 3494068590909713216L;
-	@SpringBean
-	private ServiceCliente serviceCliente;
-	
-	public AdministradorAtualizaClientePage(Cliente cliente) {
-		add(new AdmistradorAtualizaClienteForm("formularioAtualizar", cliente){
+   private static final long serialVersionUID = 3494068590909713216L;
+   @SpringBean
+   private ServiceCliente serviceCliente;
 
-			private static final long serialVersionUID = 8512385576069462696L;
+   public AdministradorAtualizaClientePage(Cliente cliente)
+   {
+      add(new AdmistradorAtualizaClienteForm("formularioAtualizar", cliente)
+      {
 
-			@Override
-			protected void atualizarCliente(Cliente cliente) {
-				serviceCliente.atualizar(cliente);
-			}
-			
-		});
-	}
-	
-	public void renderHead(IHeaderResponse response) {
-		response.render(CssHeaderItem.forUrl("css/bootstrap.css"));
-	}
+         private static final long serialVersionUID = 8512385576069462696L;
+
+         @Override
+         protected void atualizarCliente(Cliente cliente)
+         {
+            serviceCliente.atualizar(cliente);
+         }
+
+      });
+   }
+
+   @Override
+   public void renderHead(IHeaderResponse response)
+   {
+      response.render(CssHeaderItem.forUrl("css/bootstrap.css"));
+   }
 }
